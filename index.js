5a5ce388a99c21049ea2c2688edf901ffa7eb3fb
Mercy Manrique <mercymanrique@yahoo.com>
2017-08-04 16:41:00 -0700

    html and index

---
+++ index.js
@@ -0,0 +1,13 @@
+//Navigation Bar
+var nav = false;
+function show_hide_nav(id){
+var navbar = document.getElementById(id);
+
+if(nav == false){
+navbar.style.display = "block";
+nav = true;
+}else{
+navbar.style.display = "none";
+nav = false;
+}
+}
